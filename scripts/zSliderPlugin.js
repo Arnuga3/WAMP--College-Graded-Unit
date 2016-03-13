ee80961262646c750db56a16d6fc7f513bb05937
Arnuga3 <20136310@nescol.ac.uk>
2016-03-13 01:28:56 +0000

    sliding gallery
    some css added

--- scripts/zSliderPlugin.js
+++ scripts/zSliderPlugin.js
@@ -1,21 +1,23 @@
 $.fn.slider_z = function(navbarHeight) {
 	
 	/*############### ADD CSS #################*/
-
-	//number of images
+	$('html').css({'overflow-x': 'hidden'});
+	$('body').css({'overflow-x': 'hidden'});
+	$('.sliderContainer').css({'overflow-x': 'auto', 'padding-bottom': '20px'});
+	$('.sliderImages').css({'overflow-x': 'auto', 'z-index': '9'});
+	
+	//Number of images
 	var imgAmount = $(this).children().length;
 	var screenWidth = $(window).width();
 	var screenHeight = $(window).height();
-	//define gallery width variable
+	//Define gallery width variable
 	var galleryWidth = 0;
-	//height of controller
+	//Height of controller
 	var controllerHeight = 80;
-	//width of scrollbar (tested in chrome)
+	//Width of scrollbar (tested in chrome)
 	var scrollBar = 20;
 	var currentOnScreen = 0;
-	var afterTap = false;
 	var scrollSpeed = 800;
-	//var mouseScrollSpeed = 800;
 	var navbarHeight = navbarHeight;
 	var imgArr = [];
 	
@@ -30,7 +32,7 @@ $.fn.slider_z = function(navbarHeight) {
 	$('.sliderControl').css({"position": "absolute", "background-color": "white", "z-index": "10",
 	"padding-left": "20px", "width": screenWidth + "px", "height": controllerHeight + "px"});
 	
-		//set each image to the full width (vertical bar width problem - hidden but take up space)
+	//Set each image to the full width (vertical bar width problem - hidden but take up space)
 	if ($("body").height() > $(window).height()) {
 		$('.sliderControl').css({"width": screenWidth + scrollBar + "px"});
     }
@@ -38,16 +40,16 @@ $.fn.slider_z = function(navbarHeight) {
 	//Set the height of gallery to the window height minus controller width
 	$(this).children().css({"height": (screenHeight - controllerHeight - navbarHeight) + "px", "float": "left"});
 	
-	//read the width of each picture and make up the gallery width
+	//Read the width of each picture and make up the gallery width
 	$(this).children().each(function() {
 		var width = $(this).width();
 		galleryWidth += width;
 	});
 	
-	//set the gallery container
+	//Set the gallery container
 	$(this).css({"width": galleryWidth + "px"});
 	
-	//add ids to images and place them in array
+	//Add ids to images and place them in array
 	$(this).children().each(function(ind, val) {
 		
 		var img = $(this).attr('id', img + (ind + 1));
@@ -62,35 +64,27 @@ $.fn.slider_z = function(navbarHeight) {
 	});
 	
 	if ($("body").height() > $(window).height()) {
-		//set the gallery container
-		//firefox bug correction 1px
+		//Set the gallery container
+		//Firefox bug correction 1px
 		$(this).css({"width": (galleryWidth + 1) +"px"});
     }
-			/*////////////////////////////
-			///////    CONTORLS    ///////
-			////////////////////////////*/
-
-//Position tracking after touchstart!!!!!!!!!!!!!!!!!!!!!!!!!!!!Use widths of slides to track the endtouch event and slide!!!!!!!!!!!
-	/*	var leftOnTouchend = function() {
-			var onLeft = $(this).offset().left;
-			for (var i=0; i<imgArr.length; i++) {
-				if (-onLeft < imgArr[i].left) {
-					if (imgArr[i - 1].left != undefined && -onLeft > imgArr[i - 1].left) {
-						currentOnScreen = i - 1;
-						afterTap = true;
-					}
-				}
-			}
-		};
-		this.bind("mousewheel", leftOnTouchend);*/
+	
+/*////////////////////////////
+///////    CONTORLS    ///////
+////////////////////////////*/
+	
+	//CSS settings
+	$('.sliderControl img').css({'float': 'left', 'height': '60px', 'max-width': '25%', 'margin': '10px -2px',
+		'transform': 'skewX(-15deg)', 'border-top': '2px solid #ccc', 'border-bottom': '2px solid #ccc'});
+	$('.sliderControl img:first-child').css({'border-top-left-radius': '10px', 'border-left': '2px solid #ccc'});
+	$('.sliderControl img:last-child').css({'border-bottom-right-radius': '10px', 'border-right': '2px solid #ccc'});
 		
-			
-//Small picture scrolling control
+	//Small picture scrolling control
 	$(".sd23423sdfsd").on('click touchstart', function() {
 		var id = $(this).attr('id');
 		var num = parseInt(id.replace( /^\D+/g, ''));
 		currentOnScreen = num;
-		//center the image
+		//Center the image
 		var widthDiff = screenWidth - imgArr[num - 1].width;
 		var correction = widthDiff / 2;
 		
@@ -101,52 +95,4 @@ $.fn.slider_z = function(navbarHeight) {
 		}
 	});
 
-/*/Mouse scroll
-	(function () {
-		function scrollHorizontally(e) {
-			e = window.event || e;
-			var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));		//Returns-1/1 depening on the way scrolled
-			if (delta == -1) {
-				
-				if (currentOnScreen < imgAmount - 1) {
-					currentOnScreen += 1;
-				} else {
-					currentOnScreen = 0;
-					scrollSpeed = 500;
-				}
-				$('.sliderContainer').animate({scrollLeft: imgArr[currentOnScreen].left + "px"}, mouseScrollSpeed);
-				
-			} else {
-				
-				if (currentOnScreen == 0) {
-					currentOnScreen = 3;
-					scrollSpeed = 500;
-				} else {
-					if (afterTap) {
-						afterTap = false;
-					} else {
-						currentOnScreen -= 1;
-					}
-				}
-				$('.sliderContainer').animate({scrollLeft: imgArr[currentOnScreen].left + "px"}, mouseScrollSpeed);
-				
-			}
-			e.preventDefault();
-		}
-		if (window.addEventListener) {
-			// IE9, Chrome, Safari, Opera
-			window.addEventListener("mousewheel", scrollHorizontally, false);
-			// Firefox
-			window.addEventListener("DOMMouseScroll", scrollHorizontally, false);
-		} else {
-			// IE 6/7/8
-			window.attachEvent("onmousewheel", scrollHorizontally);
-		}
-	})();*/
-
-//Rebuild the sliding panel on window resize	
-	$(window).resize(function(){
-		location.reload();
-	});
-	
 };
